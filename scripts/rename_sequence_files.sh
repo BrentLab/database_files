461456ac293e7a1d04f0a95b77c2b78124450d32
cmatkhan <chase.mateusiak@gmail.com>
2020-04-30 19:09:40 -0500

    changes to scripts/rename_sequence_files.sh for run 314, 468, 480

--- scripts/rename_sequence_files.sh
+++ scripts/rename_sequence_files.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
+# this was used for a number of files. The for loop removes .txt. the rename functions replaces .fq for the most part. in some cases, needed to change first . to _ in middle of filename
+
 # remove .txt from the filenames
-for f in $1/*;
-do
-   full_path=$(realpath $f)
-   mv "$full_path" "${full_path/.txt}"
-done
+#for f in $1/*;
+#do
+#   full_path=$(realpath $f)
+#   mv "$full_path" "${full_path/.txt}"
+#done
 
 # replace extension with .fq
-rename 's/.fq//'  $1/*
+#rename 's/.fq//'  $1/*
 
 # zip
 #gzip $1/*
+
+##############################################################################
+
+# for run 314 -- replace first . in file name with _, replace .fq with .fastq and zip
+
+# remove .txt from the filenames
+#for f in $1/*;
+#do
+#   full_path=$(realpath $f)
+#   mv "$full_path" "${full_path/.txt}"
+#done
+
+# replace extension with .fq
+rename 's/\./_/'  $1/*
+rename 's/.fq/.fastq/' $1/*
+
+# zip
+gzip $1/*
+
+###############################################################################
